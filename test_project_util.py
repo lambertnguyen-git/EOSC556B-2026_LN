e5546cc5d16f3389a102c5c5eb9a832cb3560afe
Lambert Nguyen <lambert@dhcp-206-12-67-17.ubcsecure.wireless.ubc.ca>
2026-03-19 10:20:18 -0700

    start of notebook and test

---
+++ test_project_util.py
@@ -0,0 +1,14 @@
+#test functions
+
+#check dimensions of your results
+    #x_nodes = np.linspace(0,1,50)
+    #kernel = kernel_function(x_nodes,1,1,1)
+    #assert(len(kernel)) == 100
+
+#zero out your parameters
+    #test_kernels =kernel_function(p,0,0,0)
+    #check =np.cos(....)
+    #passed=np.allclose(test_kernels,check, optional--> relativetolerance (%), absolute tolerance(value))
+    #print(f"kernel max:{np.max[kernel]}, test_kernel max: {np.max[test_kernel]}")
+    #print(f"kernel min:{np.min[kernel]}, test_kernel min: {np.min[test_kernel]}")
+    #print(f"kernel norm:{np.linalg.norm[kernel]}, test_kernel norm: {np.linalg.norm[test_kernel]}",  norm diff: {np.linalg.norm[kernel]-np.linalg.norm[test_kernel]}")
